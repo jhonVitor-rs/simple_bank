bc1f3deb7c79f872981eb8e5367b017b4a59ae90
jhonVitor-rs <joaovitor.jvrs6@gmail.com>
2024-05-18 15:44:29 -0300

    feat: corrigin users views

--- lib/simple_bank_web/views/user_json.ex
+++ lib/simple_bank_web/views/user_json.ex
@@ -1,40 +1,14 @@
 defmodule SimpleBankWeb.UserJSON do
   alias SimpleBankWeb.ErrorJSON
 
+  alias SimpleBank.User
+
   def render("all_users.json", %{users: users}) do
-    %{data: for(user <- users, do: %{
-      id: user.id,
-      first_name: user.first_name,
-      last_name: user.last_name,
-      cpf: user.cpf,
-      accounts: for(account <- user.accounts, do:
-      %{number: account.number, type: account.type}
-      )
-    })}
+    %{data: users}
   end
 
-  def render("user.json", %{user: user}) do
-    %{
-      id: user.id,
-      first_name: user.first_name,
-      last_name: user.last_name,
-      cpf: user.cpf,
-      birth: user.birth,
-      address: user.address,
-      cep: user.cep,
-      accounts: for(account <- user.accounts, do:
-        %{
-          id: account.id,
-          number: account.number,
-          balance: account.balance,
-          type: account.type,
-          inserted_at: account.inserted_at,
-          updated_at: account.updated_at
-        }
-      ),
-      inserted_at: user.inserted_at,
-      updated_at: user.updated_at
-    }
+  def render("user.json", %{user: %User{} = user}) do
+    %{user: user}
   end
 
   def render("error.json", %{result: result}) do
